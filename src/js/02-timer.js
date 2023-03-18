2e309e2f2e50e06c1997c378fe5515b99cf8f17f
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-03-18 23:54:07 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -57,7 +57,7 @@ startButton.addEventListener('click', () => {
 });
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+ 
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
