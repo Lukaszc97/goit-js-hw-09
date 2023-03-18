8d11932e5bdb3f4e6b31562ccc8b1859c633fbdd
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-03-18 23:02:54 +0000

    Deploying to gh-pages from @ Lukaszc97/goit-js-hw-09@2e309e2f2e50e06c1997c378fe5515b99cf8f17f 🚀

---
+++ 01-color-switcher.f245fb76.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");a.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3)})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.f245fb76.js.map
