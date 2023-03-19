a082c7ed3f426a42b64b8c0ad746b71bb1fe3132
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-03-20 00:06:16 +0100

    chyba ostateczna wersja

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,10 +12,12 @@ let countdownInterval;
 
 flatpickr(dateTimePicker, {
   enableTime: true,
-  minDate: 'today',
-  dateFormat: 'Y-m-d H:i',
+    dateFormat: "Y-m-d H:i",
+    time_24hr: true,
+    defaultDate: new Date(),
+        minuteIncrement: 1,
   onClose: function(selectedDates) {
-    const selectedDate = selectedDates[0];
+    let selectedDate = selectedDates[0];
 
     if (!selectedDate) {
       return;
@@ -61,15 +63,24 @@ startButton.addEventListener('click', () => {
 });
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
